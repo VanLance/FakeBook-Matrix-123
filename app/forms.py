7804a5652b53d95784881cd063a62bd349d0c823
unknown <dylan23smith1990@gmail.com>
2023-07-17 15:41:57 -0500

    first commit

---
+++ app/forms.py
@@ -0,0 +1,9 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField, BooleanField
+from wtforms.validators import DataRequired
+
+class LoginForm(FlaskForm):
+    email = StringField('Email', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    remember_me = BooleanField('Remeber Me')
+    submit = SubmitField('Sign In')
