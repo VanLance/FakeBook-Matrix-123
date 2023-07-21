a88c4858c8eb10d4ef1f13a81fce0bb82d1f36fb
unknown <dylan23smith1990@gmail.com>
2023-07-21 11:13:49 -0500

    add email validation

--- app/forms.py
+++ app/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
-from wtforms.validators import DataRequired, EqualTo
+from wtforms.validators import DataRequired, EqualTo, Email
 
 class LoginForm(FlaskForm):
-    email = StringField('Email', validators=[ DataRequired()])
+    email = StringField('Email', validators=[ DataRequired(), Email() ])
     password = PasswordField('Password', validators=[ DataRequired()])
     remember_me = BooleanField('Remeber Me')
     submit = SubmitField('Sign In')
@@ -12,7 +12,7 @@ class RegisterForm(FlaskForm):
     first_name = StringField('First name')
     last_name  = StringField('Last name')
     username = StringField('Username',validators=[ DataRequired() ])
-    email = StringField('Email', validators=[ DataRequired() ])
+    email = StringField('Email', validators=[ DataRequired(), Email() ])
     password = PasswordField('Password', validators=[ DataRequired() ])
     verify_password = PasswordField('Confirm Password', validators=[ DataRequired(), EqualTo('password') ])
     submit = SubmitField('Register')
